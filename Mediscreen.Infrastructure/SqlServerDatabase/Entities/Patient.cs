f807d41847a1154a38f4505b71feae6b0065d485
Clement <clement.llobet@tutanota.com>
2024-05-25 16:27:20 +0200

    add patient entity

--- Mediscreen.Infrastructure/SqlServerDatabase/Entities/Patient.cs
+++ Mediscreen.Infrastructure/SqlServerDatabase/Entities/Patient.cs
@@ -1,12 +1,23 @@
-﻿namespace Mediscreen.Infrastructure.SqlServerDatabase.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mediscreen.Infrastructure.SqlServerDatabase.Entities;
 
 public class Patient
 {
     public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public DateTime BirthDate { get; set; }
-    public string Gender { get; set; } = string.Empty;
-    public string HomeAddress { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
+    public required string FirstName { get; set; } = string.Empty;
+    public required string LastName { get; set; } = string.Empty;
+    public required DateTime BirthDate { get; set; }
+    public required string Gender { get; set; } = string.Empty;
+    public required string HomeAddress { get; set; } = string.Empty;
+    public required string PhoneNumber { get; set; } = string.Empty;
+
+    public static void Configure(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Patient>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.ToTable(nameof(Patient));
+        });
+    }
 }
--- Mediscreen.Infrastructure/SqlServerDatabase/MediscreenSqlServerContext.cs
+++ Mediscreen.Infrastructure/SqlServerDatabase/MediscreenSqlServerContext.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mediscreen.Infrastructure.SqlServerDatabase.Entities;
+using Mediscreen.Infrastructure.SqlServerDatabase.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mediscreen.Infrastructure.SqlServerDatabase;
 
 public class MediscreenSqlServerContext : DbContext
 {
     public MediscreenSqlServerContext(DbContextOptions<MediscreenSqlServerContext> options) : base(options)
-    {
+    { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        Patient.Configure(modelBuilder);
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await base.SaveChangesAsync();
     }
 }
