0c68cfd02933220fef8ba6a4f204743fdbc614bd
Clement <clement.llobet@tutanota.com>
2024-07-19 18:20:47 +0200

    Add mongoFB Notes document

---
+++ Mediscreen.Infrastructure/MongoDbDatabase/Documents/Notes.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Mediscreen.Infrastructure.MongoDbDatabase.Documents;
+
+public class Notes
+{
+    [BsonId]
+    [BsonElement("patientId")]
+    public int PatientId { get; set; }
+    [BsonElement("note")]
+    public string Note { get; set; } = string.Empty;
+    [BsonElement("doctorId")]
+    public int DoctorId { get; set; }
+}
